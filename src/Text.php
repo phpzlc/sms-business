04e08e0fedbae4c1169228443983bb1e5732c2b0
jay <hejunjie@zmit.cn>
2020-10-26 16:06:22 +0800

    fixed:修复

---
+++ src/Text.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PhpStorm.
+ * User: Jay
+ * Date: 2020/10/26
+ */
+
+namespace PHPZlc\SmsBusiness;
+
+
+class Text
+{
+    public function text()
+    {
+        echo '这是测试';
+    }
+}
